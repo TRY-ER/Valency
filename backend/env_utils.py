c38402e661cda727665b94309bc2b6bb76d9d948
TRY-ER <avyarth00@gmail.com>
2025-02-28 10:00:52 +0530

    updated docker setup

--- backend/env_utils.py
+++ backend/env_utils.py
@@ -1,14 +1,17 @@
 import os
 
 def set_env_vars(file_path: str):
-    with open(file_path, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            try:
-                line = line.strip()
-                key = line.split("=")[0] 
-                value = line.split("=")[1] 
-                os.environ[key] = value
-            except Exception as e:
-                print(f"Error in setting env vars: {e}")
-                continue
+    try:
+        with open(file_path, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                try:
+                    line = line.strip()
+                    key = line.split("=")[0] 
+                    value = line.split("=")[1] 
+                    os.environ[key] = value
+                except Exception as e:
+                    print(f"Error in setting env vars: {e}")
+                    continue
+    except Exception as e:
+        print("Error in loading the .env file. Make sure to add the env files with corresponding paths")
