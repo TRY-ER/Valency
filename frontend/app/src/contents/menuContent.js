8b643fb6e43a76a7bf7a9ca80561236c39aba137
TRY-ER <avyarth00@gmail.com>
2025-05-17 19:15:19 +0530

    sidebar fixed

--- frontend/app/src/contents/menuContent.js
+++ frontend/app/src/contents/menuContent.js
@@ -6,12 +6,13 @@ import ToolInterface from "../components/tool_interface/ToolInterface";
 import GeneratorTabContents from "./tag_content/GeneratorTags";
 import ExplorerTabContent from "./tag_content/ExploreTags";
 import DiscriminatorTabContents from "./tag_content/DiscriminatorTags";
+import { FaCompass, FaBalanceScale, FaFlask, FaComments, FaTools } from 'react-icons/fa';
 
 const menuContent = [
     {
         id: 1,
         title: 'Explorers',
-        iconPath: '/images/explorer.png',
+        icon: <FaCompass />,
         link: '',
         description: `Explorers are used to explore molecules, proteins, and polymers.
                       This is the main tool below which there are three more tools consisting
@@ -23,19 +24,19 @@ const menuContent = [
     {
         id: 2,
         title: 'Discriminators',
-        iconPath: '/images/discriminator.png',
+        icon: <FaBalanceScale />,
         link: 'discriminators',
         description: `Discriminators tool cosists of tools to discriminate molecules, proteins, and polymers.
                       But as of now we have similarity search implemented as a sub tool. This sub tool contains
                       structural similarity search for molecules, proteins, and polymers handled as sub-sub tools.`,
         includeDocs: true,
         component: <Discriminator tabContent={DiscriminatorTabContents} basePath="discriminators" />,
-        subElements: DiscriminatorTabContents 
+        subElements: DiscriminatorTabContents
     },
     {
         id: 3,
         title: 'Generators',
-        iconPath: '/images/generator.png',
+        icon: <FaFlask />,
         link: 'generators',
         description: `Generators are used to create hypothetical molecules, polymers (protein generation is not yet implemented).
                       It has two sub-tools name BRICS and LSTM generators. Both of these tools have sub tools for generation for SMILES
@@ -47,7 +48,7 @@ const menuContent = [
     {
         id: 4,
         title: 'Chatbot',
-        iconPath: '/images/chatbot.png',
+        icon: <FaComments />,
         link: 'chatbot',
         description: 'Chatbot is used to craftfully navigate in the system.',
         component: <ChatInterface />
@@ -55,7 +56,7 @@ const menuContent = [
     {
         id: 5,
         title: 'Tool Config',
-        iconPath: '/images/tool_config.png',
+        icon: <FaTools />,
         description: 'Tool Config is used to configure the system.',
         link: 'tool-config',
         component: <ToolInterface />
